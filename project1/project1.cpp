6a48e1eb14c687b2d468bd52dec0195e9d1e2e95
lit8 <lit8@muohio.edu>
2014-09-25 10:44:31 -0400

    Tuo Li 10:56AM

---
+++ project1/project1.cpp
@@ -0,0 +1,78 @@
+//CSE609 Porject1 Tuo Li
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x1,y1,x2,y2,x3,y3;
+	cin>>y1;
+	cin>>x1;
+	cin>>y2;
+	cin>>x2;
+	if(x1<=0||y1<=0||x2<=0||y2<=0||x1!=y2)
+	{
+		cout<<"Invalid matrix."<<endl;
+		return -1;
+	}
+	int a[y1][x1];
+	int b[y2][x2];
+	x3=x2;
+	y3=y1;
+	int c[y3][x3];
+	//cout<<"a:"<<endl;
+	for(int i=0;i<y1;i++)
+	{
+		for(int j=0;j<x1;j++)
+		{
+			cin>>a[i][j];
+		}
+	}
+	//cout<<"b:"<<endl;
+	for(int i=0;i<y2;i++)
+	{
+		for(int j=0;j<x2;j++)
+		{
+			cin>>b[i][j];
+		}
+	}
+	//cout<<"c:"<<endl;
+	for(int i=0;i<y3;i++)
+	{
+		for(int j=0;j<x3;j++)
+		{
+			c[i][j]=0;
+			for(int k=0;k<y2;k++)
+			{
+				c[i][j]+=a[i][k]*b[k][j];
+			}
+		}
+	}
+	//cout<<"result:"<<endl;
+	for(int i=0;i<y1;i++)
+	{
+		for(int j=0;j<x1;j++)
+		{
+			cout<<a[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+	for(int i=0;i<y2;i++)
+	{
+		for(int j=0;j<x2;j++)
+		{
+			cout<<b[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+	for(int i=0;i<y3;i++)
+	{
+		for(int j=0;j<x3;j++)
+		{
+			cout<<c[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
